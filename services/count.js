af1f978f9db70d4088e608f237099302561288f6
p_bavaz <paul.bavazzano@gmail.com>
2021-02-23 17:16:30 +0100

    TP

--- services/count.js
+++ services/count.js
@@ -3,10 +3,39 @@ const indexName = config.get('elasticsearch.index_name');
 
 exports.count = (client, from, to, callback) => {
     // TODO Compter le nombre d'anomalies entre deux dates
-    callback({
-        count: 0
-    })
+    
+    client.count({
+                index: indexName,
+                body: {
+                    query:{ 
+                        range:  {
+                            timestamp: {
+                                gte: from,
+                                lt: to
+                            }
+                        }
+                    }
+                }
+            }).then(resp => {
+                callback({
+                    count: resp.body.count
+            })
+        })
+            
 }
+/* 
+GET /dansmarue/_count
+{
+  "query": {
+    "range": {
+      "timestamp": {
+        "gte": "2016-01-01",
+        "lt": "2018-01-01"
+      }
+    }
+  }
+}
+*/
 
 exports.countAround = (client, lat, lon, radius, callback) => {
     // TODO Compter le nombre d'anomalies autour d'un point géographique, dans un rayon donné
