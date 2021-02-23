2e86fdf4e9d08aa3c20f8920cac0a262190eb650
bart-tek <barthelemy.tek@imt-atlantique.net>
2021-02-23 13:44:35 +0100

    rendu carrez daillet tek

--- import.js
+++ import.js
@@ -8,7 +8,35 @@ async function run () {
     // Create Elasticsearch client
     const client = new Client({ node: config.get('elasticsearch.uri') });
 
-    // TODO il y a peut être des choses à faire ici avant de commencer ... 
+    // Suppression de l'index si il existe
+    if(client.indices.exists({ index: indexName})) {
+        client.indices.delete({ index: indexName }, (err, resp) => {
+        
+            if (err) console.trace(err.message);})
+    }
+
+    // Création de l'indice
+    client.indices.create({ index: indexName }, (err, resp) => {
+        
+        if (err) console.trace(err.message);
+
+        client.indices.putMapping({  
+            index: indexName,
+            body: {
+              properties: {
+                'location': {
+                  'type': 'geo_point'
+                },
+              }
+            }
+          },function(err,resp,status){
+            if (err) {
+              console.log(err);
+            }
+        });
+    });
+
+    let anos = [];
 
     // Read CSV file
     fs.createReadStream('dataset/dans-ma-rue.csv')
@@ -16,13 +44,61 @@ async function run () {
             separator: ';'
         }))
         .on('data', (data) => {
-            // TODO ici on récupère les lignes du CSV ...
-            console.log(data);
+            anos.push({
+                timestamp : data.DATEDECL,
+                object_id : data.OBJECTID,
+                annee_declaration : data["ANNEE DECLARATION"],
+                mois_declaration : data["MOIS DECLARATION"],
+                type : data.TYPE,
+                sous_type : data.SOUSTYPE,
+                code_postal : data.CODE_POSTAL,
+                ville : data.VILLE,
+                arrondissement : data.ARRONDISSEMENT,
+                prefixe : data.PREFIXE,
+                intervenant : data.INTERVENANT,
+                conseil_de_quartier : data['CONSEIL DE QUARTIER'],
+                location : data.geo_point_2d
+            });
+            
+            // console.log(data);
         })
         .on('end', () => {
-            // TODO il y a peut être des choses à faire à la fin aussi ?
-            console.log('Terminated!');
+            
+            ano_array = chunkArray(anos, 100);
+            
+            ano_array.forEach(array => {
+                client.bulk(createBulkInsertQuery(array), (err, resp) => {
+                    if (err) console.trace(err.message);
+                    else console.log(`Inserted ${resp.body.items.length} anos`);
+                });
+            }, () => {
+                client.close();
+                console.log('Terminated!');
+            });
+
         });
 }
 
+// Fonction utilitaire permettant de formatter les données pour l'insertion "bulk" dans elastic
+function createBulkInsertQuery(anos) {
+    const body = anos.reduce((acc, ano) => {
+      const { timestamp, annee_declaration, mois_declaration, type, sous_type, code_postal, ville, arrondissement, prefixe, intervenant, conseil_de_quartier, location } = ano;
+      acc.push({ index: { _index: indexName, _type: '_doc', _id: ano.object_id } })
+      acc.push({ timestamp, annee_declaration, mois_declaration, type, sous_type, code_postal, ville, arrondissement, prefixe, intervenant, conseil_de_quartier, location })
+      return acc
+    }, []);
+  
+    return { body };
+}
+
 run().catch(console.error);
+
+function chunkArray(myArray, chunk_size){
+    var results = [];
+    
+    while (myArray.length) {
+        results.push(myArray.splice(0, chunk_size));
+    }
+    
+    return results;
+}
--- services/count.js
+++ services/count.js
@@ -2,15 +2,49 @@ const config = require('config');
 const indexName = config.get('elasticsearch.index_name');
 
 exports.count = (client, from, to, callback) => {
-    // TODO Compter le nombre d'anomalies entre deux dates
-    callback({
-        count: 0
+    client.count({
+        index: indexName,
+        body: {
+            query: {
+                range: {
+                  timestamp: {
+                    gte: from,
+                    lt: to
+                  }
+                }
+            }
+        }
+    })
+    .then(resp => {
+        callback({
+            count: resp.body.count
+        })
     })
 }
 
 exports.countAround = (client, lat, lon, radius, callback) => {
     // TODO Compter le nombre d'anomalies autour d'un point géographique, dans un rayon donné
-    callback({
-        count: 0
+    client.count({
+        index: indexName,
+        body: {
+            query: {
+                bool: {
+                  must: {
+                    match_all: {}
+                  },
+                  filter: {
+                    geo_distance: {
+                      distance: radius,
+                      location: lat + "," + lon
+                    }
+                  }
+                }
+            }
+        }
+    })
+    .then(resp => {
+        callback({
+            count: resp.body.count
+        })
     })
 }
