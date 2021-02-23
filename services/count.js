2254e9371f9a46ca94e5d2f68544a6997460f9fe
Marc LAMY <marclamy.bzh@gmail.com>
2021-02-23 17:36:04 +0100

    Added count requests

--- services/count.js
+++ services/count.js
@@ -2,15 +2,56 @@ const config = require('config');
 const indexName = config.get('elasticsearch.index_name');
 
 exports.count = (client, from, to, callback) => {
-    // TODO Compter le nombre d'anomalies entre deux dates
-    callback({
-        count: 0
-    })
+    client
+        .count({
+            index: indexName,
+            body: {
+                query: {
+                    range: {
+                        '@timestamp': {
+                            gte: from,
+                            lt: to,
+                        }
+                    }
+                }
+            }
+        })
+        .then(resp => {
+            callback({
+                count: resp.body.count
+            })
+        })
+        .catch(err => {
+            console.log(errerr.message);
+        });
 }
 
 exports.countAround = (client, lat, lon, radius, callback) => {
-    // TODO Compter le nombre d'anomalies autour d'un point géographique, dans un rayon donné
-    callback({
-        count: 0
-    })
+    client
+        .count({
+            index: indexName,
+            body: {
+                query: {
+                    bool: {
+                        must: {
+                            match_all: {},
+                        },
+                        filter: {
+                            geo_distance: {
+                                distance: radius,
+                                location: { lat: lat, lon: lon }
+                            }
+                        }
+                    }
+                }
+            }
+        })
+        .then(resp => {
+            callback({
+                count: resp.body.count
+            })
+        })
+        .catch(err => {
+            console.log(err.message);
+        });
 }
