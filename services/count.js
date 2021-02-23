4598caf8e40b7f0fc0afbdeba89492d1e271a4f8
Florian-Le-Menn <florian.lemenn29@gmail.com>
2021-02-23 09:04:55 +0100

    Requete count

--- services/count.js
+++ services/count.js
@@ -2,15 +2,41 @@ const config = require('config');
 const indexName = config.get('elasticsearch.index_name');
 
 exports.count = (client, from, to, callback) => {
-    // TODO Compter le nombre d'anomalies entre deux dates
     callback({
-        count: 0
+        count: client
+            .count({
+                index: indexName,
+                body: {
+                    query: {
+                        range: {
+                            timestamp: {
+                                "gte": from,
+                                "lte": to
+                            }
+                        }
+                    }
+                }
+            })
     })
 }
 
 exports.countAround = (client, lat, lon, radius, callback) => {
     // TODO Compter le nombre d'anomalies autour d'un point géographique, dans un rayon donné
     callback({
-        count: 0
+        count: client
+            .count({
+                index: indexName,
+                body: {
+                    query: {
+                        geo_distance: {
+                            "distance": "12km",
+                            "location": {
+                                "lat": 40,//Problème, la position se trouve dans un seul champ commun, comment faire ?
+                                "lon": -70
+                            }
+                        }
+                    }
+                }
+            })
     })
 }
